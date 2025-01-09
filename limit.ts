7ed0fb3dfbcd3b1ba1ee00aea0680c01897d9d7f
paj33tooor <139661626+pajeetoor@users.noreply.github.com>
2025-01-09 00:12:41 -0500

    Create limit.ts

---
+++ limit.ts
@@ -0,0 +1,15 @@
+const sevaSeemaayeinAurSamayikVidhiyan = {
+  seemaayein: {
+    adhikSamanvaySamvaadon: 1,
+    sandeshAntar: 60000,
+    adhikDainikAPICall: 100000,
+    samvaadCoolDown: 60000,
+    vishayBadlaavSeema: 5,
+  },
+  samayikVidhiyan: {
+    minimumDer: 5000,
+    maximumDer: 10000,
+    pratikriyaDer: 5000,
+    typingSpeed: 100,
+  },
+};
